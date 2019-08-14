7f28c1bd7cd45bb9018d489752dd59eaeb49d5d1
leo <48340932+amangeyashang@users.noreply.github.com>
2019-08-14 15:00:38 +0800

    测试提交代码

--- TestCase.py
+++ TestCase.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 import click as click
 _author_ = 'Leo'
-__date__ = '2019/8/12 15:05'
+__date__ = '2019/8/14 15:05'
 
 from selenium import webdriver
 import time
@@ -36,6 +36,77 @@ driver.find_element_by_id("publicProduct").click()
 time.sleep(1)
 driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[6]').click()
 
-#退出浏览器
+#商品管理-未上架商品
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[7]').click()
+
+#订单管理-订单列表
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[9]').click()
+
+#订单管理-退货管理
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[10]').click()
+
+#订单管理-订单报表
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[11]').click()
+
+#订单管理-线下订单列表
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[12]').click()
+
+#个人账户-安全设置
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[14]').click()
+
+#评价管理-评价列表
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[16]').click()
+
+#配送管理-地址信息
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[18]').click()
+
+#配送管理-默认物流公司
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[19]').click()
+
+#配送管理-运费模版
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[20]').click()
+
+#促销管理-优惠券中心
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[22]').click()
+
+#促销管理-简单特价
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[23]').click()
+
+#促销管理-折扣促销
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[24]').click()
+
+#促销管理-买赠满赠
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[25]').click()
+
+#促销管理-加价换购
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[26]').click()
+
+#促销管理-满减促销
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[27]').click()
+
+#促销管理-拼团促销
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[28]').click()
+
+#账号管理-权限管理
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="menu-list"]/div/ul/li[30]').click()
+#浏览器退出
 driver.quit()
 
